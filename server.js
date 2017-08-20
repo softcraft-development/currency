33ead971f6416898edd22efb3b5501c32e90d33c
Craig Walker <github@softcraft.ca>
2017-08-20 16:52:05 +0000

    Format code

--- server.js
+++ server.js
@@ -10,31 +10,31 @@ var errorhandler = require('errorhandler'),
 	helmet = require('helmet'),
 
 	app = express();
-	port = 8888;
+port = 8888;
 
-	app.use(function(req, res, next) {
-		res.header('Access-Control-Allow-Credentials', true);
-		res.header('Access-Control-Allow-Origin', req.headers.origin);
-		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-		res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-			if ('OPTIONS' == req.method) {
-				res.send(200);
-			} else {
-				next();
-			}
-	});
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Credentials', true);
+	res.header('Access-Control-Allow-Origin', req.headers.origin);
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+	if ('OPTIONS' == req.method) {
+		res.send(200);
+	} else {
+		next();
+	}
+});
 
-	app.disable('x-powered-by');
-	app.use(helmet());
+app.disable('x-powered-by');
+app.use(helmet());
 
-	app.use(bodyParser.json())
-	app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
-	// routes ======================================================================
-	require('./routes.js')(app); // load our routes and pass in our app
+// routes ======================================================================
+require('./routes.js')(app); // load our routes and pass in our app
 
-	// launch ======================================================================
-	app.listen(port);
+// launch ======================================================================
+app.listen(port);
 
-	console.log('available at localhost:' + port);
+console.log('available at localhost:' + port);
