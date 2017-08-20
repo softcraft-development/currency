9691e5a59a7a84c3071a32a362135b4961a55248
Craig Walker <github@softcraft.ca>
2017-08-20 17:03:36 +0000

    Disable eslint warnings on unused parameters
    These might be used in the near future, or useful for documentation purposes. Reinvestigate later.

--- routes.js
+++ routes.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 module.exports = function (app, passport, express) {
 
 	//Routing files
@@ -10,11 +11,13 @@ module.exports = function (app, passport, express) {
 		.use('/', front)
 
 	// Handle 404
+	// eslint-disable-next-line no-unused-vars
 	app.use(function (req, res, next) {
 		res.status(404).send('<h1>ERROR 404:</h1>Page not found! OOPS!');
 	});
 
 	// Handle 500
+	// eslint-disable-next-line no-unused-vars
 	app.use(function (error, req, res, next) {
 		res.status(500).send('<h1>ERROR 500:</h1>Error loading services<br />' + error + '<br />' + error.stack);
 	});
