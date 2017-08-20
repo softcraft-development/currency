28eaba011d42575a28a57779b183371918cb8d16
Craig Walker <github@softcraft.ca>
2017-08-20 16:51:35 +0000

    Basic eslint file

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "env": {
+        "node": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module"
+    }
+};
